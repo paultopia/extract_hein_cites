16f06087c80337219068054f95839e5917f628f4
Paul Gowder <paul.gowder@gmail.com>
2022-08-30 10:13:39 -0500

    start

---
+++ extract_cite.py
@@ -0,0 +1,53 @@
+import sys
+from PyPDF2 import PdfFileReader
+# from collections import OrderedDict
+
+try:
+    to_parse = sys.argv[1]
+except:
+    raise Exception("no file given")
+
+with open(to_parse, 'rb') as infile:
+    reader = PdfFileReader(infile)
+    firstpage = reader.getPage(0).extractText()
+    
+styles = ['Bluebook 21st ed.',
+          'ALWD 7th ed.',
+          'APA 7th ed.',
+          'Chicago 17th ed.',
+          'McGill Guide 9th ed.',
+          'AGLC 4th ed.',
+          'MLA 9th ed.',
+          'OSCOLA 4th ed.',
+          'Provided by:']
+
+lines = [x.strip() for x in firstpage.split('\n')]
+
+def find_style(style):
+    next_style=styles[styles.index(style) + 1]
+    start_ind = lines.index(style)
+    end_ind = lines.index(next_style)
+    return ' '.join(lines[start_ind + 1:end_ind])
+        
+chicago = find_style('Chicago 17th ed.')
+bluebook = find_style('Bluebook 21st ed.')
+
+def find_authors():
+    authorlist = chicago.split(',')[0]
+    if ';' in authorlist:
+        return authorlist.split(';')
+    else:
+        return [authorlist]
+
+def find_pages():
+    pages = chicago.strip().split(' ')[-1]
+    indiv = pages.split('-')
+    return {'first_page': indiv[0], 'last_page': indiv[-1]}
+
+def find_year():
+    return bluebook.strip().split(' ')[-1][1:-2]
+    
+
+print(find_authors())
+print(find_pages())
+print(find_year())
